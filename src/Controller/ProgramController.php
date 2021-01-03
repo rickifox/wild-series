b5e513212c02a9a7e0e4c32bfe50dae68931a041
rickifox <rickipereira3@gmail.com>
2021-01-03 22:42:20 +0100

    created show method on Program Controller and associated show.html.twig template with all routing

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,15 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/programs", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig',[
             'website' => 'Wils Series index',
+        ]);
+    }
+    /**
+     * @Route("/{id}", methods={"GET"}, requirements={"id"="\d+"}, name="show", )
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig',[
+            'website' => 'Program '.$id,
         ]);
     }
 }
